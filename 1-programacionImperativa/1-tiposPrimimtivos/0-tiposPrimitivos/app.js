4e16b0c6126b631045acc16587853775fcbe0b3c
Setillo <setillofm@gmail.com>
2022-01-29 23:08:29 +0100

    refactor

--- 1-programacionImperativa/1-tiposPrimimtivos/0-tiposPrimitivos/app.js
+++ 1-programacionImperativa/1-tiposPrimimtivos/0-tiposPrimitivos/app.js
@@ -6,3 +6,11 @@ console.writeln(`Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 Donec rhoncus sollicitudin enim vitae tempor. 
 Nullam dui lorem, vulputate varius sapien ac, malesuada dictum metus. 
 ...`);
+console.write("abc");
+console.write(123);
+console.writeln();
+console.writeln();
+let n = console.readNumber("Numero: ");
+console.writeln("numero: " + n);
+console.writeln();
+console.writeln("string: " + console.readString("string: "));
