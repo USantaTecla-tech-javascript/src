afda380443c52aeb88a01bad9a6d96cba0bd544a
Setillo <setillofm@gmail.com>
2022-01-29 22:33:42 +0100

    refactor

--- 1-programacionImperativa/1-tiposPrimimtivos/0-tiposPrimitivos/app.js
+++ 1-programacionImperativa/1-tiposPrimimtivos/0-tiposPrimitivos/app.js
@@ -1,8 +1,8 @@
 const { Console } = require("./console");
 
 const console = new Console();
-console.writeln('Hola mundo!!!'); // Hola mundo!!!
-console.writeln(31.41592e-1);  // 3.141592
-console.writeln("31.41592e-1");  // 31.41592e-1
-console.writeln(true);  // true
-console.writeln("false");  // false
+console.writeln('Hola, mundo!!!');
+console.writeln(`Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Donec rhoncus sollicitudin enim vitae tempor. 
+Nullam dui lorem, vulputate varius sapien ac, malesuada dictum metus. 
+...`);
