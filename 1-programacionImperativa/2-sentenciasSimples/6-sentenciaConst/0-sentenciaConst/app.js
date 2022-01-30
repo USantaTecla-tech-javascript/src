8b87ad7773364686a26e97b11b320354b23db16a
chopar76 <76262061+chopar76@users.noreply.github.com>
2022-01-30 12:58:37 +0100

    refactor

--- 1-programacionImperativa/2-sentenciasSimples/6-sentenciaConst/0-sentenciaConst/app.js
+++ 1-programacionImperativa/2-sentenciasSimples/6-sentenciaConst/0-sentenciaConst/app.js
@@ -2,6 +2,6 @@ const { Console } = require("./console");
 
 const console = new Console();
 const AGE_MAJORITY = 18;
-let age = +console.readInt("Qué años tienes? "); // Qué años tienes?  18
+let age = +console.readNumber("Qué años tienes? "); // Qué años tienes?  18
 let isAdult = age >= AGE_MAJORITY;
 console.writeln("Mayor de edad? " + isAdult); // Mayor de edad? true
