8ed55d5961ade929759cebe1d0643fe1f174a00e
Setillo <setillofm@gmail.com>
2022-02-02 20:32:24 +0100

    refactor

--- 1-programacionImperativa/2-sentenciasSimples/3-sentenciaLet/app.js
+++ 1-programacionImperativa/2-sentenciasSimples/3-sentenciaLet/app.js
@@ -2,4 +2,11 @@ const { Console } = require("./console");
 
 const console = new Console();
 let identificador = 0;
+console.writeln(`.${identificador}.`); // .0.
+
 let identificadorSinInicializacion;
+console.writeln(`.${identificadorSinInicializacion}.`); // .undefined.
+
+  //console.writeln( `.${ningunIdentificador}.`); // .undefined.
+
+
