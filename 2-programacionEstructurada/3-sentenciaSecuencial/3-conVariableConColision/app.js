52cb0f144fdec68eb5dbdc877edc68661f52eea1
Setillo <setillofm@gmail.com>
2022-02-13 22:23:41 +0100

    refactor

--- 2-programacionEstructurada/3-sentenciaSecuencial/3-conVariableConColision/app.js
+++ 2-programacionEstructurada/3-sentenciaSecuencial/3-conVariableConColision/app.js
@@ -4,25 +4,22 @@ const console = new Console();
 let nivel = 100;
 console.writeln(`---`);
 nivel++;
-console.writeln(nivel);
-
+console.writeln(nivel); // 101
 {
-  let nivel = 200;    
+  let nivel = 200;
   console.writeln(`---`);
   nivel++;
-  console.writeln(nivel);
+  console.writeln(nivel); // 201
   {
     let nivel = 300;
     console.writeln(`---`);
     nivel++;
-    console.writeln(nivel);
-  }    
-   console.writeln(`---`);
-   nivel++;
-   console.writeln(nivel);
+    console.writeln(nivel); // 301
+  }
+  console.writeln(`---`);
+  nivel++;
+  console.writeln(nivel); // 202
 }
 console.writeln(`---`);
 nivel++;
-console.writeln(nivel);
-
-
+console.writeln(nivel); // 102
