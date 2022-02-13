e84cda52c36725dac572afca9286086d70c2e6c6
Setillo <setillofm@gmail.com>
2022-02-13 22:20:43 +0100

    refactor

--- 2-programacionEstructurada/3-sentenciaSecuencial/2-conVariables/app.js
+++ 2-programacionEstructurada/3-sentenciaSecuencial/2-conVariables/app.js
@@ -8,7 +8,7 @@ console.writeln(`---`);
  // nivel1++;
  // console.writeln(nivel1);
 nivel0++;
-console.writeln(nivel0);
+console.writeln(nivel0); // 101
 
 {
   let nivel1 = 200;    
@@ -16,27 +16,27 @@ console.writeln(nivel0);
   // nivel2++;
   // console.writeln(nivel2);
   nivel1++;
-  console.writeln(nivel1);
+  console.writeln(nivel1); // 201
   nivel0++;
-  console.writeln(nivel0);
+  console.writeln(nivel0); // 102
 
   {
     let nivel2 = 300;
     console.writeln(`---`);
     nivel2++;
-    console.writeln(nivel2);
+    console.writeln(nivel2); // 301
     nivel1++;
-    console.writeln(nivel1);
+    console.writeln(nivel1); // 202
     nivel0++;
-    console.writeln(nivel0);
+    console.writeln(nivel0); // 103
   }    
    console.writeln(`---`);
   // nivel2++;
   // console.writeln(nivel2);
   nivel1++;
-  console.writeln(nivel1);
+  console.writeln(nivel1); // 203
   nivel0++;
-  console.writeln(nivel0);
+  console.writeln(nivel0); // 104
 }
 console.writeln(`---`);
  // nivel2++;
@@ -44,12 +44,16 @@ console.writeln(`---`);
  // nivel1++;
  // console.writeln(nivel1);
 nivel0++;
-console.writeln(nivel0);
+console.writeln(nivel0); // 105
 
-console.writeln(`---`);
+
 {
-  console.writeln(nivel0);
-  // console.writeln(nivel1);
-  // console.writeln(nivel2);
+  console.writeln(`---`);
+ // nivel2++;
+ // console.writeln(nivel2);
+ // nivel1++;
+ // console.writeln(nivel1);
+  nivel0++;
+  console.writeln(nivel0); // 106
 }
 
