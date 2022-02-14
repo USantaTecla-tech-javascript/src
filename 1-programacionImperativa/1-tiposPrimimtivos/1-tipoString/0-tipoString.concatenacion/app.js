5df7140e057f70750d3858518cf7718a9523aee1
Setillo <setillofm@gmail.com>
2022-02-14 07:57:36 +0100

    refactor

--- 1-programacionImperativa/1-tiposPrimimtivos/1-tipoString/0-tipoString.concatenacion/app.js
+++ 1-programacionImperativa/1-tiposPrimimtivos/1-tipoString/0-tipoString.concatenacion/app.js
@@ -6,13 +6,13 @@ console.writeln("Buenas " + 'Tardes');  // Buenas Tardes
 console.writeln("Buenas " + ' Tardes');  // Buenas  Tardes
 console.writeln("1" + "2" + "3" + "...");  // 123...
 
+console.writeln("Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" +
+  "Donec rhoncus sollicitudin enim vitae tempor.\n" +
+  "Nullam dui lorem, vulputate varius sapien ac, malesuada dictum metus.\n" +
+  "...");
+
 console.writeln(
 "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n\
 Donec rhoncus sollicitudin enim vitae tempor.\n\
 Nullam dui lorem, vulputate varius sapien ac, malesuada dictum metus.\n\
 ...");
-
-console.writeln("Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" +
-              "Donec rhoncus sollicitudin enim vitae tempor.\n" + 
-              "Nullam dui lorem, vulputate varius sapien ac, malesuada dictum metus.\n" +
-              "...");
