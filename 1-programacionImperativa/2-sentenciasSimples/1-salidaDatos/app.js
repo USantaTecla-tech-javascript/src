3978e57efaad9bb2a5d62ce53c823a2fc474df16
Setillo <setillofm@gmail.com>
2022-02-15 09:07:35 +0100

    refactor

--- 1-programacionImperativa/2-sentenciasSimples/1-salidaDatos/app.js
+++ 1-programacionImperativa/2-sentenciasSimples/1-salidaDatos/app.js
@@ -1,9 +1,9 @@
 const { Console } = require("./console");
 
 const console = new Console();
-console.writeln();
-console.writeln(`otra vez`);
-console.writeln(true);
-console.writeln(1);
-console.writeln(NaN);
-console.writeln(Infinity);
+console.writeln(); //
+console.write(`pierDeN o ga`); // pierDeN o ga ...
+console.writeln(NaN); // ...NaN
+console.writeln(true); // true
+console.writeln(1); // 1
+console.writeln(Infinity); // Infinity
