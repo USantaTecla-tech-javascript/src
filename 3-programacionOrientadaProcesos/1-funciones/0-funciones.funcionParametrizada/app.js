c229658498cb5772d716a8ab29e5d4fa0fab22ba
Setillo <setillofm@gmail.com>
2022-02-16 22:29:23 +0100

    refactor

--- 3-programacionOrientadaProcesos/1-funciones/0-funciones.funcionParametrizada/app.js
+++ 3-programacionOrientadaProcesos/1-funciones/0-funciones.funcionParametrizada/app.js
@@ -2,21 +2,24 @@ const { Console } = require("./console");
 
 const console = new Console();
 function funcion(x, y=0) {
-    console.writeln("entra:")
-    console.writeln(x);
-    console.writeln(y);
+    console.write(`x: ${x}, y: ${y} => `);
     x++;
     y++;
-    console.writeln(x);
-    console.writeln(y);
+    console.writeln(`x: ${x}, y: ${y}`);
 }
 
-funcion(1,2);
-let x=1;
-let y=2;
-funcion(x,y);
-funcion(x+y*2,x*y-1);
-funcion(undefined,"012a");
-funcion(x,y,666);
-funcion(-1);
-funcion();
+funcion(); // x: undefined, y: 0 => x: NaN, y: 1
+funcion(-1); // x: -1, y: 0 => x: 0, y: 1
+funcion(undefined); // x: undefined, y: 0 => x: NaN, y: 1
+funcion(1,2); // x: 1, y: 2 => x: 2, y: 3
+let x=1; // 
+let y=2; // 
+funcion(y,x); // x: 2, y: 1 => x: 3, y: 2
+funcion(x+y*2,x*y-1); // x: 5, y: 1 => x: 6, y: 2
+funcion(x,y,666); // x: 1, y: 2 => x: 2, y: 3
+
+
+
+
+
+
