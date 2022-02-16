f49e5314c82b7b5d14573ab58ca3d756ed42f22e
Setillo <setillofm@gmail.com>
2022-02-16 22:39:15 +0100

    refactor

--- 3-programacionOrientadaProcesos/1-funciones/0-funciones.funcionParametrizada/app.js
+++ 3-programacionOrientadaProcesos/1-funciones/0-funciones.funcionParametrizada/app.js
@@ -15,6 +15,8 @@ funcion(1,2); // x:1, y:2 => x:2, y:3
 let x=1; // 
 let y=2; // 
 funcion(y,x); // x:2, y:1 => x:3, y:2
+funcion(x,y); // x:1, y:2 => x:2, y:3
+funcion(y,x); // x:2, y:1 => x:3, y:2
 funcion(x+y*2,x*y-1); // x:5, y:1 => x:6, y:2
 funcion(x,y,666); // x:1, y:2 => x:2, y:3
 
