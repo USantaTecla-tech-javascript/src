9ced41350c1edf423795c05aeecd601b1f35e30a
Setillo <setillofm@gmail.com>
2022-02-17 12:36:34 +0100

    refactor

--- 2-programacionEstructurada/4-tablas/0-tablas.modificacion/app.js
+++ 2-programacionEstructurada/4-tablas/0-tablas.modificacion/app.js
@@ -2,11 +2,23 @@ const { Console } = require("./console");
 
 const console = new Console();
 let array = [1,2,3,4,5];
-console.writeln(array);
-console.writeln("length: " + array.length);
+console.writeln(`${array} con ${array.length} items`); // 1,2,3,4,5 con 5 items 
 array[0] = 0;
-console.writeln(array);
-console.writeln("length: " + array.length);
-array[4] = 6;
-console.writeln(array);
-console.writeln("length: " + array.length);
+console.writeln(`${array} con ${array.length} items`); // 0,2,3,4,5 con 5 items
+array = [`a`, `b`, `c`];
+console.writeln(`${array} con ${array.length} items`); // a,b,c con 3 items
+array[0] = `_`;
+console.writeln(`${array} con ${array.length} items`); // _,b,c con 3 items
+array = [];
+console.writeln(`${array} con ${array.length} items`); //  con 0 items
+
+let bidimensional = [
+  [1,2,3],
+  [],
+  [true, false]
+];
+bidimensional[0] = bidimensional[1];
+bidimensional[1] = bidimensional[2];
+bidimensional[2] = [3,2,1];
+console.writeln(`${bidimensional} con ${bidimensional.length} items`); // ,true,false,3,2,1 con 3 items
+
