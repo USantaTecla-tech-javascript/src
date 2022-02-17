9113c188251b6c3af03a6ec5f5336a8983f8986e
Setillo <setillofm@gmail.com>
2022-02-17 08:28:58 +0100

    refactor

--- 2-programacionEstructurada/4-tablas/0-tablas.alta/app.js
+++ 2-programacionEstructurada/4-tablas/0-tablas.alta/app.js
@@ -3,39 +3,32 @@ const { Console } = require("./console");
 const console = new Console();
 let array = [];
 for(let item of array){
-    console.writeln(item);
+    console.write(`${item}, `); // 
 }
-console.writeln("length: " + array.length);
+console.writeln(` with length: ${array.length}`); // with length: 0
 
 array[0] = 10;
 for(let item of array){
-    console.writeln(item);
+    console.write(`${item}, `); // 10,  ...
 }
-console.writeln("length: " + array.length);
+console.writeln(` with length: ${array.length}`); // ... with length: 1
 
 array[1] = 11;
 for(let item of array){
-    console.writeln(item);
+    console.write(`${item}, `); // 10, 11, ... 
 }
-console.writeln("length: " + array.length);
+console.writeln(` with length: ${array.length}`); // ... with length: 2
 
-array[99] = 990;
+array[9] = 99;
 for(let item of array){
-    console.writeln(item);
+    console.write(`${item}, `); // 10, 11, undefined, undefined, undefined, undefined, undefined, undefined, undefined, 99, ...
 }
-console.writeln("length: " + array.length);
+console.writeln(` with length: ${array.length}`); // ... with length: 10
 
 array[-1] = -10;
 for(let item of array){
-    console.writeln(item);
+    console.write(`${item}, `); // 10, 11, undefined, undefined, undefined, undefined, undefined, undefined, undefined, 99, ...
 }
-console.writeln(array[-1]); 
-console.writeln("length: " + array.length);
+console.writeln(` with length: ${array.length}`); // ... with length: 10
+console.writeln(array[-1]); // -10
 
-array["zzz"] = "zzz";
-for(let item of array){
-    console.writeln(item);
-}
-console.writeln(array[-1]);
-console.writeln(array["zzz"]);
-console.writeln("length: " + array.length);
