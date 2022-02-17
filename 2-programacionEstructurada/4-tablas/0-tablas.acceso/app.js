4c4c7c5768e229227527d150403e3fadf80875c5
Setillo <setillofm@gmail.com>
2022-02-17 12:22:01 +0100

    refactor

--- 2-programacionEstructurada/4-tablas/0-tablas.acceso/app.js
+++ 2-programacionEstructurada/4-tablas/0-tablas.acceso/app.js
@@ -2,25 +2,28 @@ const { Console } = require("./console");
 
 const console = new Console();
 let array = [1,2,3,4,5];
-console.writeln(typeof array);
-console.writeln(array[0]);
-console.writeln(typeof array[0]);
-console.writeln(array[4]);
-console.writeln(typeof array[4]);
-console.writeln(array[5]);
-console.writeln(typeof array[5]);
-for(let i = 0; i < array.length; i++){
-    console.writeln(array[i]);
-}
-for(let item of array){
-    console.writeln(item);
-}
+console.writeln(typeof array); // object
+console.writeln(array[0]); // 1
+console.writeln(typeof array[0]); // number
+console.writeln(array[4]); // 5
+console.writeln(typeof array[4]); // number
+console.writeln(array[array.length]); // 
+console.writeln(typeof array[array.length]); // undefined
+
+console.writeln([[`a`, `b`, `c`],[1,2],[true]][1][1]); // 2
+console.writeln(typeof [[`a`, `b`, `c`],[1,2],[true]][1][1]); // number
+console.writeln([[`a`, `b`, `c`],[1,2],[true]][1]); // 1,2
+console.writeln(typeof [[`a`, `b`, `c`],[1,2],[true]][1]); // object
+console.writeln([[`a`, `b`, `c`],[1,2],[true]]); // a,b,c,1,2,true
+console.writeln(typeof [[`a`, `b`, `c`],[1,2],[true]]); // object
 
 for(let i = 0; i < [1,2,3].length; i++){
-    console.writeln([1,2,3][i]);
+    console.write(`${[1,2,3][i]}, `); // 1,2,3
 }
+console.writeln();
 for(let item of [1,2,3]){
-    console.writeln(item);
+    console.write(`${item}, `); // 1,2,3
 }
+console.writeln();
 
 
