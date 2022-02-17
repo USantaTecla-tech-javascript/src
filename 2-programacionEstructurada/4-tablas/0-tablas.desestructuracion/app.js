0fc0b421b19e5b0e5bd625869bf46dd9ac8a15ab
Setillo <setillofm@gmail.com>
2022-02-17 08:09:35 +0100

    refactor

--- 2-programacionEstructurada/4-tablas/0-tablas.desestructuracion/app.js
+++ 2-programacionEstructurada/4-tablas/0-tablas.desestructuracion/app.js
@@ -2,10 +2,21 @@ const { Console } = require("./console");
 
 const console = new Console();
 let [head, ...rest] = [1,2,3,4,5];
-console.writeln(head);
-console.writeln(rest);
+console.writeln(head); // 1
+console.writeln(rest); // 2,3,4,5
 
-let array = ["a", "e", "i", "o", "u"];
-[head, ...rest] = array;
-console.writeln(head);
-console.writeln(rest);
+[head, ...rest] = ["a", "e", "i", "o", "u"];
+console.writeln(head); // a
+console.writeln(rest); // e,i,o,u
+[head, ...rest] = [true];
+console.writeln(head); // true
+console.writeln(rest); // []
+[head, ...rest] = [];
+console.writeln(head); // undefined
+console.writeln(rest); // undefined
+ // [head, ...rest] = null;
+
+let [[first, second], ...tail] = [[1,2],[3],4];
+console.writeln(first); // 1
+console.writeln(second); // 2
+console.writeln(tail); // 3,4
