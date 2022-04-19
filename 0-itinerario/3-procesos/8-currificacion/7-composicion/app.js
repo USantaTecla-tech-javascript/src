4c8bbed7e3e4e91adbd8c473f12da8ee01bb0d85
Setillo <setillofm@gmail.com>
2022-04-19 11:49:47 +0200

    refactor

--- 0-itinerario/3-procesos/8-currificacion/7-composicion/app.js
+++ 0-itinerario/3-procesos/8-currificacion/7-composicion/app.js
@@ -1,6 +1,29 @@
 const { Console } = require("./console");
 
 const console = new Console();
+const list = [
+  {
+      id: 1,
+      name: 'Steve',
+      email: 'steve@example.com',
+  },
+  {
+      id: 2,
+      name: 'John',
+      email: 'john@example.com',
+  },
+  {
+      id: 3,
+      name: 'Pamela',
+      email: 'pam@example.com',
+  },
+  {
+      id: 4,
+      name: 'Liz',
+      email: 'liz@example.com',
+  },
+];
+
 const compose = (f, g) => (x) => f(g(x));
 
 
