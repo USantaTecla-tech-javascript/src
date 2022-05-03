57974b846388a82ab558938dbb896282b9d0eb88
Setillo <setillofm@gmail.com>
2022-05-03 07:12:39 +0200

    refactor

--- 0-itinerario/4-objetos/12-iteradores/x-fibonacci/0-iterador/app.js
+++ 0-itinerario/4-objetos/12-iteradores/x-fibonacci/0-iterador/app.js
@@ -38,38 +38,3 @@ while(!item.done){
 }
 console.writeln(`:${msg.slice(0, msg.length - 2)}.`);
 
-
-const Fibonacci = (maxIndex) => (
-  {
-    [Symbol.iterator]: () => {
-      let index = 0;
-      let previous = 0;
-      let current = 1;
-      return {
-        next: () => {
-          let result = {
-            done: index === maxIndex
-          };
-          if (!result.done) {
-            [previous, current] = [current, (previous + current)];
-            result = {
-              value: previous
-            };
-            index++;
-          }
-          return result;
-        }
-      }
-    }
-  }
-);
-msg = ``;
-for (let item of Fibonacci(1)) {
-  msg += `${item}, `;
-}
-console.writeln(`:${msg.slice(0, msg.length - 2)}.`);
-msg = ``;
-for (let item of Fibonacci(10)) {
-  msg += `${item}, `;
-}
-console.writeln(`:${msg.slice(0, msg.length - 2)}.`);
---
+++ 0-itinerario/4-objetos/12-iteradores/x-fibonacci/1-iterable/app.js
@@ -0,0 +1,37 @@
+const { Console } = require("./console");
+
+const console = new Console();
+const Fibonacci = (maxIndex) => (
+  {
+    [Symbol.iterator]: () => {
+      let index = 0;
+      let previous = 0;
+      let current = 1;
+      return {
+        next: () => {
+          let result = {
+            done: index === maxIndex
+          };
+          if (!result.done) {
+            [previous, current] = [current, (previous + current)];
+            result = {
+              value: previous
+            };
+            index++;
+          }
+          return result;
+        }
+      }
+    }
+  }
+);
+msg = ``;
+for (let item of Fibonacci(1)) {
+  msg += `${item}, `;
+}
+console.writeln(`:${msg.slice(0, msg.length - 2)}.`);
+msg = ``;
+for (let item of Fibonacci(10)) {
+  msg += `${item}, `;
+}
+console.writeln(`:${msg.slice(0, msg.length - 2)}.`);
