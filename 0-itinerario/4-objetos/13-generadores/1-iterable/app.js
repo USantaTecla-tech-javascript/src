a94d4e89fe6b28a6da7e5cb644b5597ae4bd1bf9
Setillo <setillofm@gmail.com>
2022-05-03 11:43:40 +0200

    refactor

--- 0-itinerario/4-objetos/13-generadores/1-iterable/app.js
+++ 0-itinerario/4-objetos/13-generadores/1-iterable/app.js
@@ -15,15 +15,3 @@ for (let item of iterableObject) {
 for (let item of [...iterableObject, ...iterableObject]) {
   console.writeln(item); // 1,2,3,1,2,3
 }
-
-// function* hacedorIds() {
-//   var indice = 0;
-//   while (true)
-//     yield indice++;
-// }
-// var gen = hacedorIds();
-
-// console.log(gen.next().value); // 0
-// console.log(gen.next().value); // 1
-// console.log(gen.next().value); // 2
-// // ...
