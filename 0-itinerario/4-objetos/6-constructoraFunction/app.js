ec2cb1ad56bc5cb6c0667167fb740620617f9580
Setillo <setillofm@gmail.com>
2022-10-03 17:25:44 +0200

    refactor

--- 0-itinerario/4-objetos/6-constructoraFunction/app.js
+++ 0-itinerario/4-objetos/6-constructoraFunction/app.js
@@ -25,11 +25,11 @@ superior(funcUser, 1000); // Antes [ attribute: Value ] Despues
 let numbers = [5, 6, 2, 3, 7];
 let max = Math.max(...numbers);
 let min = Math.min(...numbers);
-console.writeln(`Max: ${max} - Min: ${min}`);
+console.writeln(`Max: ${max} - Min: ${min}`); // Max: 7 - Min: 2
 max = Math.max.apply(null, numbers);
 min = Math.min.apply(null, numbers);
-console.writeln(`Max: ${max} - Min: ${min}`);
+console.writeln(`Max: ${max} - Min: ${min}`); // Max: 7 - Min: 2
 [max, min] = [Math.max, Math.min].map(f => f.apply(null, numbers));
-console.writeln(`Max: ${max} - Min: ${min}`);
+console.writeln(`Max: ${max} - Min: ${min}`); // Max: 7 - Min: 2
 [max, min] = [Math.max, Math.min].map(f => f(...numbers));
-console.writeln(`Max: ${max} - Min: ${min}`);
+console.writeln(`Max: ${max} - Min: ${min}`); // Max: 7 - Min: 2
