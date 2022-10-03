e6308b449e5eca767fd6fe1127cf7be111a04ed1
Setillo <setillofm@gmail.com>
2022-10-03 17:21:01 +0200

    refactor

--- 0-itinerario/4-objetos/6-constructoraFunction/app.js
+++ 0-itinerario/4-objetos/6-constructoraFunction/app.js
@@ -2,4 +2,35 @@ const { Console } = require("./console");
 
 const console = new Console();
 
+let object = {
+    attribute: "Value",
+    write() {
+        console.write(`attribute: ${this.attribute}`);
+    }
+};
 
+let superior = function (part){
+    console.write(`Antes [ `);
+    part();
+    console.writeln(` ] Despues`);
+}
+superior(object.write, 1000); // Antes [ attribute: undefined ] Despues
+superior(function () {
+    object.write(); // Antes [ attribute: Value ] Despues
+}, 1000);
+superior(() => object.write(), 1000); // Antes [ attribute: Value ] Despues
+let funcUser = object.write.bind(object);
+superior(funcUser, 1000); // Antes [ attribute: Value ] Despues
+ 
+
+let numbers = [5, 6, 2, 3, 7];
+let max = Math.max(...numbers);
+let min = Math.min(...numbers);
+console.writeln(`Max: ${max} - Min: ${min}`);
+max = Math.max.apply(null, numbers);
+min = Math.min.apply(null, numbers);
+console.writeln(`Max: ${max} - Min: ${min}`);
+[max, min] = [Math.max, Math.min].map(f => f.apply(null, numbers));
+console.writeln(`Max: ${max} - Min: ${min}`);
+[max, min] = [Math.max, Math.min].map(f => f(...numbers));
+console.writeln(`Max: ${max} - Min: ${min}`);
--- 0-itinerario/4-objetos/8-constructorasEstructuras/3-weakMap/app.js
+++ 0-itinerario/4-objetos/8-constructorasEstructuras/3-weakMap/app.js
@@ -1,3 +1,23 @@
 const { Console } = require("./console");
 
 const console = new Console();
+let weakMap = new WeakMap();
+let array = [1, true, 'cadena de caracteres']; 
+weakMap.set("a", array);
+weakMap.set("b", array);
+weakMap.set("c", [1, true, 'cadena de caracteres']);
+let object = {a: 1, b: 2};
+weakMap.add("d", object);
+weakMap.add("e", {a: 1, b: 2});
+console.writeln(weakMap.has(array)); // true
+console.writeln(weakMap.has([1, true, 'cadena de caracteres'])); // false
+console.writeln(weakMap.has(object)); // true
+console.writeln(weakMap.has({a: 1, b: 2})); // false
+
+array = null;
+object = null;
+console.writeln(weakMap.size); // 0
+console.writeln(weakMap.has(array)); // false
+console.writeln(weakMap.has([1, true, 'cadena de caracteres'])); // false
+console.writeln(weakMap.has(object)); // false
+console.writeln(weakMap.has({a: 1, b: 2})); // false
